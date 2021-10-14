6c37e64e34eef25dfdfe01175b4df9a711d5961c
A.Skriabin <A.Skriabin@asntl.ru>
2021-10-14 18:50:01 +0300

    Connect to mongo

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const PORT = config.get('port') || 5000
 async function start() {
   try {
     await mongoose.connect(config.get('mongoUri'), {
-
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true
     })
+
+    app.listen(PORT, () => console.log(`App has been started on port... ${PORT}`))
   } catch (e) {
-    console.log('Server Error')
+    console.log('Server Error', e.message)
     process.exit(1)
   }
 }
 
 start()
 
-app.listen(PORT, () => console.log(`App has been started on port... ${PORT}`))
