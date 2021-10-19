6580028a3bacee5475be32b3ede0a1b36e7f496a
A.Skriabin <A.Skriabin@asntl.ru>
2021-10-19 18:21:10 +0300

    Add Loader component

---
+++ client/src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => (
+  <div className="wrap-loader">
+    <div className="progress">
+      <div className="indeterminate"/>
+    </div>
+  </div>
+)
