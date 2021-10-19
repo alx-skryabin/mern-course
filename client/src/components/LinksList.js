65721965b5a6fe9ca261962d5582c50c0cc639fa
A.Skriabin <A.Skriabin@asntl.ru>
2021-10-19 19:28:09 +0300

    Add LinksList component. Page 'Links'

---
+++ client/src/components/LinksList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export const LinksList = ({links}) => {
+  if (!links.length) {
+    return (
+      <p className="center-text">There are no links yet</p>
+    )
+  }
+
+  return (
+    <>
+      <table>
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>Origin</th>
+          <th>Short</th>
+          <th>Open</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        {links.map((link, index) => {
+          return (
+            <tr key={link._id}>
+              <td>{index + 1}</td>
+              <td>{link.from}</td>
+              <td>{link.to}</td>
+              <td>
+                <Link to={`/detail/${link._id}`}>Open</Link>
+              </td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+    </>
+  )
+}
